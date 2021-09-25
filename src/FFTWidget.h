1e2d94859d78e761a5b75d28d694088a06ccc8a7
Viktor Drobot <linux776@gmail.com>
2021-09-25 21:43:15 +0300

    Reworked project structure. Get rid of old sources. Add authoring/licensing info

---
+++ src/FFTWidget.h
@@ -0,0 +1,43 @@
+/*
+ * This file is part of glrpt.
+ *
+ * glrpt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * glrpt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with glrpt. If not, see https://www.gnu.org/licenses/
+ *
+ * Author: Viktor Drobot
+ */
+
+/**************************************************************************************************/
+
+#ifndef FFTWIDGET_H
+#define FFTWIDGET_H
+
+/**************************************************************************************************/
+
+#include <QWidget>
+
+/**************************************************************************************************/
+
+class FFTWidget : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit FFTWidget(QWidget *parent = nullptr);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+};
+
+/**************************************************************************************************/
+
+#endif
